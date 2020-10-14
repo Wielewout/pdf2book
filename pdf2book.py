343e5f70c5f6643e06d36b2fbbc40b57cfb9cda1
Wout Van De Wiel <vandewiel.wout@gmail.com>
2020-10-14 09:22:00 +0200

    Add split functionality

--- pdf2book.py
+++ pdf2book.py
@@ -8,8 +8,27 @@ def main(source):
         os.mkdir('output')
 
     pages = PdfReader(source).pages
+    split_pages = split_all(pages)
 
-    write_to_pdf('output/digital.' + os.path.basename(source), pages)
+    write_to_pdf('output/digital.' + os.path.basename(source), split_pages)
+
+
+def split_all(pages):
+    split_pages = list()
+    for page in pages:
+        left_page, right_page = split(page)
+        split_pages.append(left_page)
+        split_pages.append(right_page)
+    return split_pages
+
+
+def split(page):
+    x, y, width, height = page.MediaBox
+    if width > height:
+        for x_pos in (0, 0.5):
+            yield PageMerge().add(page, viewrect=(x_pos, 0, 0.5, 1)).render()
+    else:
+        yield page
 
 
 def write_to_pdf(destination, pages):
